3de138bbb062469e3ba112363d7a2d651c2f9965
Kwon-YJ <kyj1435@gmail.com>
2025-09-05 21:54:15 +0900

    [update] TODO 작성 완료

--- src/arbitrage_detector.py
+++ src/arbitrage_detector.py
@@ -24,7 +24,7 @@ class ArbitrageDetector:
         # 주요 토큰들 (차익거래 시작점)
         self.base_tokens = [
             "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",  # WETH
-            "0xA0b86a33E6441b8e5c7F5c8b5e8b5e8b5e8b5e8b",  # USDC
+            "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",  # USDC (수정됨)
             "0x6B175474E89094C44Da98b954EedeAC495271d0F",  # DAI
             "0xdAC17F958D2ee523a2206206994597C13D831ec7",  # USDT
         ]
